976c4321af0d8d0c34a3812026c0a1498dbf7623
Jangalanagababu42 <60348426+Jangalanagababu42@users.noreply.github.com>
2024-09-16 19:31:25 +0530

    fix:bg-image

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 // src/App.js
-import React from 'react';
-import Header from './components/Header';
-import HeroSection from './components/HeroSection';
-import StatsSection from './components/StatsSection';
-import NetworksCarousel from './components/NetworksCarousel';
-import Solutions from './components/Solutions';
-import ContactForm from './components/ContactForm';
-import Footer from './components/Footer';
-import solutionsbg from './assets/solutions-bg.svg';
+import Header from "./components/Header";
+import HeroSection from "./components/HeroSection";
+import NetworksCarousel from "./components/NetworksCarousel";
+import Solutions from "./components/Solutions";
+import ContactForm from "./components/ContactForm";
+import Footer from "./components/Footer";
+import solutionsbg from "./assets/vector.png";
 
 function App() {
   return (
@@ -20,7 +18,7 @@ function App() {
         className="w-full bg-no-repeat "
         style={{
           backgroundImage: `url(${solutionsbg})`,
-          backgroundSize: 'cover',
+          backgroundSize: "cover",
         }}
       >
         <Solutions />
